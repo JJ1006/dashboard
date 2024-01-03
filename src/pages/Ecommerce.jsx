96525a0c472a1cb0120e7ef5642376afc37cc29a
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2024-01-02 21:31:04 -0500

    update

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -103,7 +103,12 @@ const Ecommerce = () => {
         />
         </div>
 
+        <div className='mt-10'>
+        <Button color="white" bgColor="blue" text="Download Report" borderRadius="10px" />
         </div>
+
+        </div>
+        <Stacked width="320px" height="360px" />
         </div>
 
         </div>
